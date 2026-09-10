084f11962a95192d3df7708b7340e6323880692a
stackamit <info.stackamit@gmail.com>
2026-09-10 16:17:45 +0530

    fix netlify cors client url

--- server/server.js
+++ server/server.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'https://stackamit.netlify.app', 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
